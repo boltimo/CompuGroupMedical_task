9384f051ab900f65b32379a6f9692c638caaa4ac
boltimo <timo.bolenz@gmx.net>
2022-07-22 11:38:48 +0200

    Third Commit

--- test/samples/CompuGroup.ts
+++ test/samples/CompuGroup.ts
@@ -1,6 +1,6 @@
-describe('Name of the group', () => {
-    it('should behave...', async () => {
-        browser.url("https://demo.clickdoc.de/cd-de/")                  //go to URL test123
+describe('CompuGroupMedical Step1', () => {
+    it('navigate to the url of the application, click on the "doctor search" button', async () => {
+        browser.url("https://demo.clickdoc.de/cd-de/")                  //go to URL
         await browser.pause(2000)
         await browser.maximizeWindow()                                  //maximize the browser window
         await browser.pause(2000)
@@ -11,12 +11,19 @@ describe('Name of the group', () => {
         await  $('=Arztsuche').click()                                  //click on "Arztsuche"
         await browser.pause(2000)
 
+    });
+
+});
+
+describe('CompuGroupMedical Step2', () => {
+    it('insert the searchterm in the "searchtearm" inputfield, click on the "search" button', async () => {
+
         await $('#search-query-typeahead').setValue("Peter Test")       //insert "Peter Test"
 
         await browser.pause(2000)
 
         await browser.keys("\uE007");                                   //press ENTER to start the search
-        await browser.pause(20000)
+        await browser.pause(5000)
     });
 
 });
